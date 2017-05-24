c6501deefdb441984fbbc8306db1e9b95e36c103
Jyun-NengJi <jyunnengji@gmail.com>
2017-05-24 23:06:59 +0800

    Describe the function of ModOpLib.h.

---
+++ ModuloOp/ModOpLib.cpp
@@ -0,0 +1,79 @@
+/*	The function of ModOpLib.h
+
+	date: 2017/05/02
+*/
+#include "ModOpLib.h"
+
+int ExtendedEuclideanAlgo(int a, int b)	// find the inverse of b mod a.
+{
+	int q, r, x, x1, x2, y, y1, y2, ori_a;
+
+	x2 = 1;
+	x1 = 0;
+	y2 = 0;
+	y1 = 1;
+	ori_a = a;
+
+	while(b > 0)
+	{
+		q = a / b;
+		r = a % b;
+		x = x2 - q * x1;
+		y = y2 - q * y1;
+		x2 = x1;
+		x1 = x;
+		y2 = y1;
+		y1 = y;
+		a = b;
+		b = r;
+	}
+	y = y2;
+	y = (y < 0) ? ori_a + y : y;	// let the inverse is between 0 to a. 
+	return y;
+}
+
+bool ModOpLib::IsCongruence(int a, int b, int modulus)
+{
+	bool result;
+	
+	result = (((a - b) % modulus) == 0) ? true : false;
+	return result;
+}
+
+int *ModOpLib::FindCongruence(int a, int modulus, int numbers)
+{
+	int *ArrayPtr = new int [numbers];
+	for (int i = 1; i <= numbers; ++i)
+	{
+		ArrayPtr[i-1] = (a % modulus) + (modulus * i);
+	}
+	return ArrayPtr;
+}
+
+int ModOpLib::GCD(int a, int b)
+{
+	int big_num, small_num;
+	int q, r;
+
+	big_num = (a > b) ? a : b;
+	small_num = (big_num == a) ? b : a;
+
+	// Euclidean algorithm
+	while(small_num > 0){
+		r = big_num % small_num;
+		big_num = small_num;
+		small_num = r;
+	}
+
+	return big_num;
+}
+
+int ModOpLib::Inverse(int a, int modulus)
+{
+	int inverse = ExtendedEuclideanAlgo(modulus, a);
+
+	return inverse;
+}
+
+
+
